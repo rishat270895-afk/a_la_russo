7aa1d7d60358fd82b5c87dabbee32d1359ad01c7
rishat270895-afk <rishat270895@gmail.com>
2026-04-08 17:43:41 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -29,10 +29,10 @@ from aiogram.types import (
 # =========================
 # НАСТРОЙКИ
 # =========================
-BOT_TOKEN = "PASTE_BOT_TOKEN_HERE"
-ADMIN_IDS = {123456789}
-RESET_PASSWORD = "1234"
-CHANNEL_LINK = "https://t.me/your_channel_here"
+BOT_TOKEN = "8428046405:AAFISFm6Mm3ZStV93DsyxhZzc9HwMN6n63c"
+ADMIN_IDS = {922603146}
+RESET_PASSWORD = "12345678"
+CHANNEL_LINK = "https://t.me/nikaestate_sochi"
 
 DB_PATH = "database.db"
 EXPORTS_DIR = Path("exports")
